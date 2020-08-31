7c79231b1f2723d368e5e8ae3fdd8f060aa973cb
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2020-08-31 23:30:16 +0200

    update to angular 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatDatepickerModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatNativeDateModule,
-  MatSelectModule,
-  MatButtonModule,
-  MatProgressBarModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 import { PackagistComponent } from './packagist/packagist.component';
